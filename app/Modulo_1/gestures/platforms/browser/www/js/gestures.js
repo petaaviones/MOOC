b93ab74af3e255d15d0be2dbc52c0b28fddf803a
Ruben <r.ramosfg@gmail.com>
2017-02-04 18:14:56 +0100

    gestos leccion 2
    Se añaden los cambios en el codigo de la segunda leccion del mooc

--- app/Modulo_1/gestures/platforms/browser/www/js/gestures.js
+++ app/Modulo_1/gestures/platforms/browser/www/js/gestures.js
@@ -1,26 +1,46 @@
 var app={
 	inicio: function(){
-		
-		var botonClaro = document.querySelector('#claro');
-		var botonOscuro = document.querySelector('#oscuro');
-		
-		botonClaro.addEventListener('click', this.ponloClaro, false);
-		botonOscuro.addEventListener('click', this.ponloOscuro, false);
+		this.iniciaBotones();
+		this.iniciaFastClick();
+		this.iniciaHammer();
 	},
 	
+	iniciaFastClick: function(){
+		FastClick.attach(document.body);
+	},
+	
+	iniciaBotones: function(){
+		var botonClaro = document.querySelector("#claro");
+		var botonOscuro = document.querySelector("#oscuro");
+		
+		botonClaro.addEventListener('click', this.ponloClaro, false);
+		botonOscuro.addEventListener('click', app.ponloOsucro, false);
+	},
+	
+	iniciaHammer: function(){
+		var zona = document.getElementById('zona-gestos');
+		var hammertime = new Hammer(zona);
+
+		hammertime.get('pinch').set({enable: true});
+		hammertime.get('rotate').set({enable: true});
+
+		hammertime.on('tap doubletap swipe press rotate', function(ev){
+			document.querySelector("#info").innerHTML = ev.type+'!';
+		});
+	},
+
 	ponloClaro: function(){
 		document.body.className = 'claro';
 	},
-	
-	ponloOscuro: function(){
+
+	ponloOsucro: function(){
 		document.body.className = 'oscuro';
 	},
-	
+
 };
 
 if ('addEventListener' in document) {
-    document.addEventListener('DOMContentLoaded', function() {
-        FastClick.attach(document.body);
+    document.addEventListener('DOMContentLoaded', function() {    
 		app.inicio();
     }, false);
 }
