9f81228c9e0d22c5c91af8ff6af434b307b6ff2b
Ruben <r.ramosfg@gmail.com>
2017-02-05 23:07:08 +0100

    Paso 3 Aplicacion con filtros
    Modulo 2 paso 3 aplicacion de la camara con filtros

--- app/Modulo_2/camera/platforms/browser/www/js/camera.js
+++ app/Modulo_2/camera/platforms/browser/www/js/camera.js
@@ -1,16 +1,27 @@
 var app={
 	inicio: function(){
 		this.iniciaFastClick();
-		this.iniciaBoton();
+		this.iniciaBotones();
 	},
 	
 	iniciaFastClick: function(){
 		FastClick.attach(document.body);
 	},
 
-	iniciaBoton: function(){
+	iniciaBotones: function(){
 		var buttonAction = document.querySelector("#button-action");
 		buttonAction.addEventListener('click', this.tomarFoto);
+
+		var filterButtons = document.querySelectorAll('.button-filter');
+		filterButtons[0].addEventListener("click", function(){
+			app.aplicarFiltro('gray');
+		});
+		filterButtons[1].addEventListener("click", function(){
+			app.aplicarFiltro('negative');
+		});
+		filterButtons[2].addEventListener("click", function(){
+			app.aplicarFiltro('sepia');
+		});
 	},
 
 	tomarFoto: function(){
@@ -25,15 +36,37 @@ var app={
 	},
 
 	fotoTomada: function(imageURI){
-		var image = document.querySelector("#foto");
-		image.src = imageURI;
+		var img = document.createElement('img');
+		img.onload = function(){
+			app.pintarFoto(img);
+		}
+		img.src = imageURI;
+	},
+
+	pintarFoto: function(img){
+		var canvas = document.querySelector("#foto");
+		var context = canvas.getContext("2d");
+		canvas.width = img.width;
+		canvas.height = img.height;
+		context.drawImage(img, 0 , 0, img.width, img.height);
 	},
 
 	errorAlTomarFoto: function(message) {
 		console.log("Fallo al tomar foto o toma cancelada: "+message);
+	},
+
+	aplicarFiltro: function(filterName){
+		var canvas = document.querySelector("#foto");
+		var context = canvas.getContext("2d");
+		imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+
+		effects[filterName](imageData.data);
+
+		context.putImageData(imageData, 0, 0);
 	}
 };
 
+var imageData;
 if ('addEventListener' in document) {
     document.addEventListener('DOMContentLoaded', function() {    
 		app.inicio();
