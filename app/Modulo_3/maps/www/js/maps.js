d7b6b4ec5359960e076d1cbf8b1f2e29adbb9208
Ruben <r.ramosfg@gmail.com>
2017-02-07 23:08:49 +0100

    Paso 2 de la aplicacion

--- app/Modulo_3/maps/www/js/maps.js
+++ app/Modulo_3/maps/www/js/maps.js
@@ -12,9 +12,13 @@ var app={
 		navigator.geolocation.getCurrentPosition(app.dibujaCoordenadas, app.errorAlSolicitarLocalizacion);
 	},
 
-	dibujaCoordenadas: function(position){
-		var coordsDiv = document.querySelector("#coords");
-		coordsDiv.innerHTML = "Latitud: "+position.coords.latitude + " Longitud: "+ position.coords.longitude;
+	pintaCoordenadasEnMapa: function(position){
+		var miMapa = L.map('map').setView([position.coords.latitude, position.coords.longitude], 13);
+
+		L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ29vdHlmZXIiLCJhIjoiY2l1MGlrb2M3MDAwMDJ6bXAxY3dlOXdkYiJ9.RBfUsuzHfLrofEyMR8IVlA', {
+	      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+	      maxZoom: 18
+	    }).addTo(miMapa);
 	},
 
 	errorAlSolicitarLocalizacion: function(error) {
