0ed72208ec75af40a28f2bcfa58674e7968bb314
Ruben <r.ramosfg@gmail.com>
2017-02-13 01:50:23 +0100

    Modulo 4 Paso 3
    Modulo 4 juego con los sensores completos.

--- app/Modulo_4/game/www/js/juego.js
+++ app/Modulo_4/game/www/js/juego.js
@@ -1,10 +1,10 @@
 var app={
   inicio: function(){
     DIAMETRO_BOLA = 50;
-
-    velocidadX=0;
-    velocidadY=0;
-    puntuacion=0;
+    dificultad = 0;
+    velocidadX = 0;
+    velocidadY = 0;
+    puntuacion = 0;
     
     alto  = document.documentElement.clientHeight;
     ancho = document.documentElement.clientWidth;
@@ -20,21 +20,29 @@ var app={
 
       game.stage.backgroundColor = '#f27d0c';
       game.load.image('bola', 'assets/bola.png');
+      game.load.image('objetivo', 'assets/objetivo.png');
     }
 
     function create() {
       scoreText = game.add.text(16, 16, puntuacion, { fontSize: '100px', fill: '#757676' });
+      
+      objetivo = game.add.sprite(app.inicioX(), app.inicioY(), 'objetivo');
       bola = game.add.sprite(app.inicioX(), app.inicioY(), 'bola');
+      
       game.physics.arcade.enable(bola);
+      game.physics.arcade.enable(objetivo);
+
       bola.body.collideWorldBounds = true;
       bola.body.onWorldBounds = new Phaser.Signal();
       bola.body.onWorldBounds.add(app.decrementaPuntuacion, this);
     }
 
     function update(){
-      bola.body.velocity.y = (velocidadY * 300);
-      bola.body.velocity.x = (velocidadX * -300);
+      var factorDificultad = (300 + (dificultad * 100));
+      bola.body.velocity.y = (velocidadY * factorDificultad);
+      bola.body.velocity.x = (velocidadX * (-1 * factorDificultad));
       
+      game.physics.arcade.overlap(bola, objetivo, app.incrementaPuntuacion, null, this);
     }
 
     var estados = { preload: preload, create: create, update: update };
@@ -44,6 +52,18 @@ var app={
   decrementaPuntuacion: function(){
     puntuacion = puntuacion-1;
     scoreText.text = puntuacion;
+  },
+
+  incrementaPuntuacion: function(){
+    puntuacion = puntuacion+1;
+    scoreText.text = puntuacion;
+
+    objetivo.body.x = app.inicioX();
+    objetivo.body.y = app.inicioY();
+
+    if (puntuacion > 0){
+      dificultad = dificultad + 1;
+    }
   },
 
   inicioX: function(){
@@ -93,7 +113,7 @@ var app={
 };
 
 if ('addEventListener' in document) {
-    document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('deviceready', function() {
         app.inicio();
     }, false);
 }
