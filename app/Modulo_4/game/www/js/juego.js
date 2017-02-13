01e89d570636f2be019ae030a5e4b8d43c2c2283
Ruben <r.ramosfg@gmail.com>
2017-02-13 01:30:13 +0100

    Paso 2 Modulo 4 MOOC Sensores
    Un pequeño juego de con los sensores mover la bola.

--- app/Modulo_4/game/www/js/juego.js
+++ app/Modulo_4/game/www/js/juego.js
@@ -1,44 +1,99 @@
 var app={
   inicio: function(){
+    DIAMETRO_BOLA = 50;
 
+    velocidadX=0;
+    velocidadY=0;
+    puntuacion=0;
+    
+    alto  = document.documentElement.clientHeight;
+    ancho = document.documentElement.clientWidth;
+    
+    app.vigilaSensores();
+    app.iniciaJuego();
+  },
+
+  iniciaJuego: function(){
+
+    function preload() {
+      game.physics.startSystem(Phaser.Physics.ARCADE);
+
+      game.stage.backgroundColor = '#f27d0c';
+      game.load.image('bola', 'assets/bola.png');
+    }
+
+    function create() {
+      scoreText = game.add.text(16, 16, puntuacion, { fontSize: '100px', fill: '#757676' });
+      bola = game.add.sprite(app.inicioX(), app.inicioY(), 'bola');
+      game.physics.arcade.enable(bola);
+      bola.body.collideWorldBounds = true;
+      bola.body.onWorldBounds = new Phaser.Signal();
+      bola.body.onWorldBounds.add(app.decrementaPuntuacion, this);
+    }
+
+    function update(){
+      bola.body.velocity.y = (velocidadY * 300);
+      bola.body.velocity.x = (velocidadX * -300);
+      
+    }
+
+    var estados = { preload: preload, create: create, update: update };
+    var game = new Phaser.Game(ancho, alto, Phaser.CANVAS, 'phaser',estados);
+  },
+
+  decrementaPuntuacion: function(){
+    puntuacion = puntuacion-1;
+    scoreText.text = puntuacion;
+  },
+
+  inicioX: function(){
+    return app.numeroAleatorioHasta(ancho - DIAMETRO_BOLA );
+  },
+
+  inicioY: function(){
+    return app.numeroAleatorioHasta(alto - DIAMETRO_BOLA );
+  },
+
+  numeroAleatorioHasta: function(limite){
+    return Math.floor(Math.random() * limite);
+  },
+
+  vigilaSensores: function(){
+    
     function onError() {
         console.log('onError!');
     }
 
-    navigator.accelerometer.watchAcceleration(this.onSuccess, onError,{ frequency: 1000 });
-  },
+    function onSuccess(datosAceleracion){
+      app.detectaAgitacion(datosAceleracion);
+      app.registraDireccion(datosAceleracion);
+    }
 
-  onSuccess: function(datosAceleracion){
-    app.detectaAgitacion(datosAceleracion);
-    app.representaValores(datosAceleracion);
+    navigator.accelerometer.watchAcceleration(onSuccess, onError,{ frequency: 10 });
   },
 
   detectaAgitacion: function(datosAceleracion){
-    agitacionX = datosAceleracion.x > 10;
-    agitacionY = datosAceleracion.y > 10;
+    var agitacionX = datosAceleracion.x > 10;
+    var agitacionY = datosAceleracion.y > 10;
 
     if (agitacionX || agitacionY){
-      document.body.className = 'agitado';
-    }else{
-      document.body.className = '';
+      setTimeout(app.recomienza, 1000);
     }
   },
 
-  representaValores: function(datosAceleracion){
-    app.representa(datosAceleracion.x, '#valorx');
-    app.representa(datosAceleracion.y, '#valory');
-    app.representa(datosAceleracion.z, '#valorz');
+  recomienza: function(){
+    document.location.reload(true);
   },
 
-  representa: function(dato, elementoHTML){
-    redondeo = Math.round(dato * 100) / 100
-    document.querySelector(elementoHTML).innerHTML= redondeo;
+  registraDireccion: function(datosAceleracion){
+    velocidadX = datosAceleracion.x ;
+    velocidadY = datosAceleracion.y ;
   }
 
 };
 
 if ('addEventListener' in document) {
-    document.addEventListener('deviceready', function() {
+    document.addEventListener('DOMContentLoaded', function() {
         app.inicio();
     }, false);
 }
